a1cb41bab9e1516ad848a54a9fc84d3a0de47415
leeyunhome <leeyunhome@daum.net>
2020-03-31 14:28:49 +0900

    3-11 PureComponent, memo

--- lecture/Try_hooks.jsx
+++ lecture/Try_hooks.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+// import React from 'react';
+import React, { PureComponent, memo } from 'react';
+
+// class Try extends PureComponent {
+//     render() {
+//         console.log('렌더링');
+//         const { tryInfo } = this.props;
+//         return (
+//             <li>
+//                 <div>{tryInfo.try}</div>
+//                 <div>{tryInfo.result}</div>
+//             </li>
+//         );
+//     }
+// }
 
 const Try = ({ tryInfo }) => {
     return (
+        // console.log('렌더링'),
+
         <li>
             <div>{tryInfo.try}</div>
             <div>{tryInfo.result}</div>
--- lecture/client.jsx
+++ lecture/client.jsx
@@ -8,7 +8,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { hot } from 'react-hot-loader/root';
 
-import NumberBaseball from './RenderTest';
+import NumberBaseball from './NumberBaseball_hooks';
 
 const Hot = hot(NumberBaseball);
 
