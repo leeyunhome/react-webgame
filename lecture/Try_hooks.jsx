e9c69e6f3e0b5349b764ba8543a37ab7da48a315
leeyunhome <leeyunhome@daum.net>
2020-03-31 16:28:27 +0900

    3-13 connect props to state

--- lecture/Try_hooks.jsx
+++ lecture/Try_hooks.jsx
@@ -1,28 +1,41 @@
 // import React from 'react';
 import React, { PureComponent, memo } from 'react';
 
-// class Try extends PureComponent {
-//     render() {
-//         console.log('렌더링');
-//         const { tryInfo } = this.props;
-//         return (
-//             <li>
-//                 <div>{tryInfo.try}</div>
-//                 <div>{tryInfo.result}</div>
-//             </li>
-//         );
-//     }
-// }
+class Try extends PureComponent {
+    constructor(props) {
+        super(props);
+        // 다른 동작
+        const filtered = this.props.filter(() => {
+            
+        });
 
-const Try = ({ tryInfo }) => {
-    return (
-        // console.log('렌더링'),
+        this.state = {
+            result: this.props.result,
+            try: this.props.try,
+        };
+    }
 
-        <li>
-            <div>{tryInfo.try}</div>
-            <div>{tryInfo.result}</div>
-        </li>
-    )
-};
+    render() {
+        console.log('렌더링');
+        const { tryInfo } = this.props;
+        return (
+            <li>
+                <div>{tryInfo.try}</div>
+                <div>{tryInfo.result}</div>
+            </li>
+        );
+    }
+}
+
+// const Try = ({ tryInfo }) => {
+//     return (
+//         // console.log('렌더링'),
+
+//         <li>
+//             <div>{tryInfo.try}</div>
+//             <div>{tryInfo.result}</div>
+//         </li>
+//     )
+// };
 
 export default Try;
